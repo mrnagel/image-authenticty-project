8fd6b957a4c464a8bd2b2a6762f159956477a8c4
Matth <matthew.r.nagel@outlook.com>
2026-02-15 14:45:37 -0700

    clean up UI

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config'
-import Aura from '@primeuix/themes/aura'
+import Aura from '@primeng/themes/aura'
 
 import { routes } from './app.routes';
 
--- client/src/app/features/upload/upload.ts
+++ client/src/app/features/upload/upload.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { FileUploadModule, FileUpload } from 'primeng/fileupload';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-upload',
-  imports: [FileUpload],
+  imports: [FileUpload, ButtonModule, FileUploadModule, ProgressBarModule],
   templateUrl: './upload.html',
   styleUrl: './upload.scss',
 })
