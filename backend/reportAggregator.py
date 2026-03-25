2b97d8c58c90d9db66332da3f735e7055e46fb33
Allingsen <allingsen87@gmail.com>
2026-03-25 15:57:40 -0600

    Added initial confidence alg

--- backend/reportAggregator.py
+++ backend/reportAggregator.py
@@ -20,6 +20,39 @@ class reportAggregator:
         
         return json.dumps(return_json, indent=4)
 
+    def calculateConfidence(self, data_json):
+        def inverse_h(p: np.array):
+            '''Binary Entropy Function'''
+            h_x = (-p*np.log2(p)) - (1-p)*np.log2(1-p)
+            # 1- h_x to invert the outputs, meaning 0.5 is 0 not 1
+            return 1 - h_x
+        
+        data = json.loads(data_json)
+        models = data.keys()
+
+        p_fake = np.array([])
+        for i in models:
+            p_fake = np.append(p_fake, data[i].get('p_fake', -1))
+
+        if -1 in p_fake:
+            raise(ValueError, 'Not all models have p_fake!')
+        
+        # False if model predicts real, True if fake
+        preds = np.array([True if i > 0.5 else False for i in p_fake])
+        init_confidence = inverse_h(p_fake)
+
+        if np.all(preds == True) or np.all(preds == False):
+            # Case where all models agree
+            conf = np.average(init_confidence)
+            pred = preds[0]
+        else:
+            # TODO: create a penalizer function
+            conf = 0
+            pred = True
+        
+        return pred, conf
+
 if __name__ == "__main__":
     aggregator = reportAggregator()
-    aggregator.aggregateResults()
+    json_data = aggregator.aggregateResults()
+    print(aggregator.calculateConfidence(json_data))
