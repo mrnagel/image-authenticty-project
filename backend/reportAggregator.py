ff87f27790ad575fffd7b3c8bee1fcd65d89845e
Allingsen <allingsen87@gmail.com>
2026-04-06 17:14:21 -0600

    Changed confidence calculation

--- backend/reportAggregator.py
+++ backend/reportAggregator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pathlib
 import json
+from collections import Counter
 
 class reportAggregator:
     def __init__(self, modelPath='./model_outputs'):
@@ -17,10 +18,17 @@ class reportAggregator:
             
             with np.load(npz_file[0]) as data:
                 return_json[i] = {key: data[key].tolist() for key in data.files if data[key].ndim == 0}
-        
+
+        prediction, confidence = self.calculateConfidence(json.dumps(return_json))
+
+        print(type(prediction), type(confidence))
+        return_json['predictions'] = {
+            'prediction': prediction,
+            'confidence': confidence
+        }
         return json.dumps(return_json, indent=4)
 
-    def calculateConfidence(self, data_json):
+    def calculateConfidence2(self, data_json):
         def inverse_h(p: np.array):
             '''
             Binary Entropy Function
@@ -42,6 +50,8 @@ class reportAggregator:
         
         # False if model predicts real, True if fake
         preds = np.array([True if i > 0.5 else False for i in p_fake])
+
+        p_fake = np.clip(p_fake, 1e-10, 1-1e-10)
         init_confidence = inverse_h(p_fake)
 
         if np.all(preds == True) or np.all(preds == False):
@@ -53,7 +63,32 @@ class reportAggregator:
             conf = 0
             pred = True
         
-        return pred, conf, p_fake.tolist()
+        return pred, conf
+    
+    def calculateConfidence(self, data_json):
+        data = json.loads(data_json)
+        models = data.keys()
+
+        p_fake = np.array([])
+        for i in models:
+            p_fake = np.append(p_fake, data[i].get('p_fake', -1))
+
+        if -1 in p_fake:
+            raise ValueError('Not all models have p_fake!')
+        
+        # False if model predicts real, True if fake
+        preds = np.array([True if i > 0.5 else False for i in p_fake])
+
+        p_fake = np.clip(p_fake, 1e-10, 1-1e-10)
+        p = np.average(p_fake)
+        
+        #Binary Entropy Function (https://en.wikipedia.org/wiki/Binary_entropy_function)
+        h_x = (-p*np.log2(p)) - (1-p)*np.log2(1-p)
+        conf = 1 - h_x
+
+        pred = Counter(preds).most_common(1)[0][0]
+
+        return bool(pred), float(conf)
     
     def calculateMetrics(self):
         json_data = self.aggregateResults()
@@ -61,7 +96,7 @@ class reportAggregator:
     
 def main():
     aggregator = reportAggregator()
-    print(aggregator.calculateMetrics())
+    print(aggregator.aggregateResults())
 
 if __name__ == "__main__":
     main()
